7ffeff9c3c776dad5e93e23a4f37722c340b4e7e
AnastasiiaVld <anastasiia94cherniavska@gmail.com>
2023-03-30 23:02:52 +0200

    hm 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function createImgMarkup() {
+  galleryItems.map(img =>
+    galleryEl.insertAdjacentHTML(
+      'beforeend',
+      `
+        <a class="gallery__item" href="${img.original}">
+            <img
+                class="gallery__image"
+                src="${img.preview}"
+                alt="${img.description}"
+                title="${img.description}"
+            />
+        </a>
+        `
+    )
+  );
+}
+
+createImgMarkup();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
